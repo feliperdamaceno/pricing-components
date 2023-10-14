d3be625bf10c876d76d1c03cfd67e636a3a9e4ac
feliperdamaceno <feliperdamaceno@sent.com>
2023-10-14 20:13:03 +0100

    feat: Implement TabList logic and styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { PricingCard } from './components'
+import { PricingCard, TabList } from './components'
 
 export default function App() {
   return (
     <main>
       <PricingCard />
+
+      <div className="flow">
+        <h3 className="text-center text-neutral-100">What is your use case?</h3>
+        <TabList options={['B2C', 'B2B', 'B2E']} onClick={() => alert('🐙')} />
+      </div>
     </main>
   )
 }
--- src/components/TabList.tsx
+++ src/components/TabList.tsx
@@ -1,13 +1,28 @@
+import { useState } from 'react'
+
 interface TabListProps {
   options: string[]
   onClick: (...args: unknown[]) => unknown
 }
 
 export default function TabList({ options, onClick }: TabListProps) {
+  const [active, setActive] = useState(0)
+
+  const handleClick = (index: number) => {
+    setActive(index)
+    onClick()
+  }
+
   return (
     <div className="tab_list">
-      {options.map((tab) => (
-        <button onClick={onClick}>{tab}</button>
+      {options.map((tab, index) => (
+        <button
+          key={index}
+          className={index === active ? 'active' : ''}
+          onClick={() => handleClick(index)}
+        >
+          {tab}
+        </button>
       ))}
     </div>
   )
